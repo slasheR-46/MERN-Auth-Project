3190f570d21347765cb2b9351c34906fe1e9dcf3
slasheR-46 <gdiaz.pabloignacio@gmail.com>
2025-01-03 01:13:59 -0300

    instalacion y configuracion de React JS y Tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <>
+      <h1 className='text-2xl text-red-500 font-bold text-center mt-4'>MERN, AUTH PROJECT!</h1>
+      <p className='text-center'>Energizado por Vite + React + Tailwind CSS app.</p>
+    </>
+  );
+}
